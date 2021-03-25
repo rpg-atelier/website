ec0e1a15dca12b5f9a54f5ad2c341024abd38b26
rygos <rygos@icloud.com>
2021-03-25 10:07:59 +0100

    update composer and npm packages. install jetstream with lightwire. create homestead development enviroment

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
-        App\Providers\JetstreamServiceProvider::class,
+        //App\Providers\JetstreamServiceProvider::class,
 
     ],
 
